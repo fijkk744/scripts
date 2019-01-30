1d31edb243f7889a53fa19b8d37365ed79c55e83
fijkk744 <47195930+fijkk744@users.noreply.github.com>
2019-01-30 18:56:56 -0500

    Update debug.js

--- debug.js
+++ debug.js
@@ -10,7 +10,7 @@ const Debug = {
   check(obj, type) {
     if (obj === null || obj === void 0)
       throw new InputError("first argument is " + obj);
-    if (typeof type != "function")
+    if (!this.is(type, Function))
       throw new InputError("second argument is not a function");
     if (!this.is(obj, type))
       throw new InputError("expected " + type.name + ", got " + obj.constructor.name);
