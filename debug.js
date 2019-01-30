47c657d7668d20ecf2a0bde72911e97ccf462fba
fijkk744 <47195930+fijkk744@users.noreply.github.com>
2019-01-30 18:07:08 -0500

    Update debug.js

--- debug.js
+++ debug.js
@@ -1,20 +1,14 @@
 "use strict";
-class InputError extends Error {
-  constructor(msg) {
-    super();
-    this.name = "InputError";
-    if (msg) this.message = msg;
-  }
-}
+const InputError = function(msg) {
+  this.name = "InputError";
+  if (msg) this.message = msg;
+};
+InputError.prototype = Error.prototype;
 const Debug = {
   check(obj, type) {
-    if (obj === null || obj === void 0)
-      throw new InputError("first argument is " + obj);
-    if (typeof type != "function")
-      throw new InputError("second argument is not a function");
-    if (obj instanceof type ||
-    typeof obj == type.name.toLowerCase())
-      return;
+    if (obj === null || obj === void 0) throw new InputError("first argument is " + obj);
+    if (typeof type != "function") throw new InputError("second argument is not a function");
+    if (obj instanceof type || typeof obj == type.name.toLowerCase()) return;
     throw new InputError("expected " + type.name + ", got " + obj.constructor.name);
   }
 };
