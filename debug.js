9f25ca93f07cf21c7f7dd97c7984efbaa6c09d6a
fijkk744 <47195930+fijkk744@users.noreply.github.com>
2019-01-30 18:52:23 -0500

    Update debug.js

--- debug.js
+++ debug.js
@@ -12,7 +12,12 @@ const Debug = {
       throw new InputError("first argument is " + obj);
     if (typeof type != "function")
       throw new InputError("second argument is not a function");
-    if (!(obj instanceof type || typeof obj == type.name.toLowerCase()))
+    if (!this.is(obj, type))
       throw new InputError("expected " + type.name + ", got " + obj.constructor.name);
+  },
+  is(obj, type) {
+    if (obj === null || obj === void 0)
+      return false;
+    return obj instanceof type || typeof obj == type.name.toLowerCase();
   }
 };
