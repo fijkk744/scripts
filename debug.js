0e8744b84e2c82b6016bf130fac97908f05bf117
fijkk744 <47195930+fijkk744@users.noreply.github.com>
2019-01-30 18:13:30 -0500

    Update debug.js

--- debug.js
+++ debug.js
@@ -7,9 +7,11 @@ class InputError extends Error {
 }
 const Debug = {
   check(obj, type) {
-    if (obj === null || obj === void 0) throw new InputError("first argument is " + obj);
-    if (typeof type != "function") throw new InputError("second argument is not a function");
-    if (obj instanceof type || typeof obj == type.name.toLowerCase()) return;
-    throw new InputError("expected " + type.name + ", got " + obj.constructor.name);
+    if (obj === null || obj === void 0)
+      throw new InputError("first argument is " + obj);
+    if (typeof type != "function")
+      throw new InputError("second argument is not a function");
+    if (!(obj instanceof type || typeof obj == type.name.toLowerCase()))
+      throw new InputError("expected " + type.name + ", got " + obj.constructor.name);
   }
 };
