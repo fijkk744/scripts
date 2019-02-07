58516a65a85bec214ad712724fabd79ae4b794e0
fijkk744 <47195930+fijkk744@users.noreply.github.com>
2019-02-07 17:27:13 -0500

    Update debug.js

--- debug.js
+++ debug.js
@@ -15,6 +15,13 @@ const Debug = {
     if (!this.is(obj, type))
       throw new InputError("expected " + type.name + ", got " + obj.constructor.name);
   },
+  multicheck() {
+    let a = arguments;
+    if (a.length < 2)
+      throw new InputError("not enough arguments");
+    for (let i = 0; i < a.length - 1; i++)
+      this.check(a[i], a[a.length - 1]);
+  },
   is(obj, type) {
     if (obj === null || obj === void 0 || typeof type != "function") return false;
     return obj instanceof type || typeof obj == type.name.toLowerCase();
